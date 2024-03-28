8ec65caf876418c6f78cce4ecf18e442d0fd9271
RodrigoAncavil <rodrigo.ancavil@yahoo.com>
2024-03-28 17:07:35 -0300

    AppImage: added info about image shape

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import cv2
 
 img = cv2.imread("aerial-landscape.png")
 crop = img[100:200,100:300]
-print(type(img), type(crop))
+print(type(img), img.shape)
 
 img = cv2.resize(img, (0,0), fx=1.5, fy=1.5)
 crop = cv2.resize(crop, (0,0),fx=2.5, fy=2.5)
